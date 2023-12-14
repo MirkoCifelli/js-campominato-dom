33af1f9ed63463e05725e0800c0bdcc6dad4539a
MirkoCifelli <ciffo4@hotmail.it>
2023-12-14 11:40:26 +0100

    Iniziallizato progetto

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-console.log(`hello world`)
+const play = document.querySelector("button")
+console.log('play', play, typeof play)
+
+const diff = document.getElementById('difficulty');
+
+const container = document.querySelector('main > .grid-container')
+    console.log('container', container, typeof container)
+
+play.addEventListener('click', function(){
+    const cellNumber = parseInt(diff.value);
+    
+    container.innerHTML= '';
+
+    for (let i = 1; i <= cellNumber; i++){
+        const cell = document.createElement('div');
+        cell.innerHTML=  i ;
+        cell.classList.add('cell');
+        cell.classList.add('cell-' + cellNumber);
+        cell.addEventListener('click', function(){
+            this.classList.add('active');
+            console.log(this.innerHTML);
+        });
+        container.append(cell);
+     }
+})
+
+
